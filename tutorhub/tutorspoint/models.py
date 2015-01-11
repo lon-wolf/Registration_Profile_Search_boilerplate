c0aca8fbc17e16a9b1f78bcbbe1dafd2d94dfd15
Shivam <vermashivam1994@gmail.com>
2015-01-12 02:13:53 +0530

    changes in templates

--- tutorhub/tutorspoint/models.py
+++ tutorhub/tutorspoint/models.py
@@ -4,12 +4,12 @@ from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 
 class Teacher_Profile(models.Model):
-	user = models.ForeignKey(User, unique=True)
-	first_name = models.CharField(max_length = 30, blank = True)
-	last_name = models.CharField(max_length = 30, blank = True)
-	qual = models.CharField(max_length = 30, blank = True)
+	user = models.ForeignKey(User, unique = True)
+	first_name = models.CharField("First Name", max_length = 30, blank = True)
+	last_name = models.CharField("Last Name", max_length = 30, blank = True)
+	qual = models.CharField("Qualification", max_length = 30, blank = True)
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-	phone_number = models.CharField(validators=[phone_regex], blank=True, max_length = 15)
+	phone_number = models.CharField("Phone Number", validators=[phone_regex], blank=True, max_length = 15)
 
 	def __unicode__(self):
 		return self.first_name
@@ -17,11 +17,11 @@ class Teacher_Profile(models.Model):
 
 class Student_Profile(models.Model):
 	user = models.ForeignKey(User, unique=True)
-	first_name = models.CharField(max_length = 30, blank = True)
-	last_name = models.CharField(max_length = 30, blank = True)
-	qual = models.CharField(max_length = 30, blank = True)
+	first_name = models.CharField("First Name", max_length = 30, blank = True)
+	last_name = models.CharField("Last Name", max_length = 30, blank = True)
+	qual = models.CharField("Qualification", max_length = 30, blank = True)
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-	phone_number = models.CharField(validators=[phone_regex], blank=True, max_length = 15)
+	phone_number = models.CharField("Phone Number", validators=[phone_regex], blank=True, max_length = 15)
 
 	def __unicode__(self):
 		return self.first_name
